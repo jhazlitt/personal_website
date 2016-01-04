9d832743af8a51768c22772e02c9ae688df18b44
root <root@ip-172-31-20-37.us-west-2.compute.internal>
2016-01-04 05:28:45 +0000

    Added the directories for the rest of the portfolio

--- js/index.js
+++ js/index.js
@@ -12,7 +12,7 @@ document.getElementById("portfolio_button").onclick = function() {
 "<table id='project_table'><tr><td><a href='floppy_bird_game/floppy_bird.html'><img src='img/floppy_bird_logo.png' id='project_logo'></img></a></td><td><a href='floppy_bird_game/floppy_bird.html'><h1>Blocky Bird</h1></a><h3>A Flappy Bird clone.  Made with HTML, CSS, JavaScript, and jQuery.</h3></td></tr>" + 
 "<table id='project_table'><tr><td><a href='snake_game/snake.html'><img src='img/snake_logo.png' id='project_logo'></img></a></td><td><a href='snake_game/snake.html'><h1>Snake</h1></a><h3>A Snake clone.  Made with HTML, CSS, JavaScript, and jQuery.</h3></td></tr>" + 
 "<table id='project_table'><tr><td><a href='color_match_game/color_match.html'><img src='img/color_match_logo.png' id='project_logo'></img></a></td><td><a href='color_match_game/color_match.html'><h1>Color Match</h1></a><h3>A game where you match colored tiles.  Made with HTML, CSS, JavaScript, and jQuery.</h3></td></tr>" +
-"<table id='project_table'><tr><td><a href='../ball_dodge_game/ball_dodge.html'><img src='img/ball_dodge_logo.png' id='project_logo'></img></a></td><td><a href='../ball_dodge_game/ball_dodge.html'><h1>Ball Dodge</h1></a><h3>My first (admittedly not great) attempt at a game with jQuery.  Made with HTML, CSS, JavaScript, and jQuery.</h3></td></tr>"; 
+"<table id='project_table'><tr><td><a href='ball_dodge_game/ball_dodge.html'><img src='img/ball_dodge_logo.png' id='project_logo'></img></a></td><td><a href='ball_dodge_game/ball_dodge.html'><h1>Ball Dodge</h1></a><h3>My first (admittedly not great) attempt at a game with jQuery.  Made with HTML, CSS, JavaScript, and jQuery.</h3></td></tr>"; 
 
 }
 
